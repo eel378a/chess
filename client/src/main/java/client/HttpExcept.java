b5e2e32f4cdb70484f1370341c8c8d6a23edaa41
Lexie Lee <lexielee378@gmail.com>
2025-05-30 12:00:04 -0600

    http exception class, extends runtimeException

---
+++ client/src/main/java/client/HttpExcept.java
@@ -0,0 +1,24 @@
+package client;
+import com.google.gson.Gson;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+public class HttpExcept extends RuntimeException{
+    final private int statusCode;
+    public HttpExcept(int statusCode, String message) {
+        super(message);
+        this.statusCode = statusCode;
+    }
+
+    public static HttpExcept fromStream(InputStream stream) {
+        var map = new Gson().fromJson(new InputStreamReader(stream), HashMap.class);
+        int status = ((Double)map.get("status")).intValue();
+        String message = map.get("message").toString();
+        return new HttpExcept(status, message);
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+}
